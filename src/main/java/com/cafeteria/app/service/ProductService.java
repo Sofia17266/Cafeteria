362991dd9fd4ad3f5d4aa9bac82154e552618756
Josué Brenes <camicarmo1110@gmail.com>
2024-12-03 09:41:07 -0600

    ProductService

--- src/main/java/com/cafeteria/app/service/ProductService.java
+++ src/main/java/com/cafeteria/app/service/ProductService.java
@@ -15,19 +15,19 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public List<Product> getProductsByCategoryId(Long categoryId) {
-        return productRepository.findByCategoryIdAndSubcategoryIsNull(categoryId);
-    }
-
-    public List<Product> getProductsBySubcategoryId(Long subcategoryId) {
-        return productRepository.findBySubcategoryId(subcategoryId);
-    }
-
     public List<Product> getProductsByIds(List<Long> ids) {
         return productRepository.findByIdIn(ids);
     }
 
     public Optional<Product> getProductById(Long productId) {
         return productRepository.findById(productId);
+    }
+
+    public List<Product> getProductsByCategoryId(Long categoryId) {
+        return productRepository.findByCategoryIdAndSubcategoryIsNull(categoryId);
+    }
+
+    public List<Product> getProductsBySubcategoryId(Long subcategoryId) {
+        return productRepository.findBySubcategoryId(subcategoryId);
     }
 }
